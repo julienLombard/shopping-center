2e52c38c69a04863de2e192d62dcfaa3e2823141
julienLombard <julienlombard.fr@gmail.com>
2022-07-13 17:07:50 +0200

    Adding Layout component

---
+++ components/Layout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+export const Layout = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title ? title : 'Shopping-Center'}</title>
+        <meta name="description" content="E-commerce web sites Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between ">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">Shopping Center</a>
+            </Link>
+            <div>
+              <Link href="/first-shop">
+                <a className="p-2">First-shop</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Shopping-Center - 2022</p>
+        </footer>
+      </div>
+    </>
+  );
+};
